8928fe7654ed6f6c8d9395bc98c0f01b92fec9bb
sarath <sarathcreativethinker@gmail.com>
2017-02-18 18:53:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sarath/tab/MyFragment.java
@@ -0,0 +1,43 @@
+package com.example.sarath.tab;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyFragment extends Fragment {
+
+
+    public MyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+     View v =inflater.inflate(R.layout.fragment_my,container,false);
+
+        TextView tv = (TextView) v.findViewById(R.id.textView);
+        tv.setText(getArguments().getString("msg"));
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+
+    public static MyFragment newInstance(String text){
+        MyFragment f= new MyFragment();
+        Bundle b= new Bundle();
+        b.putString("msg",text);
+        f.setArguments(b);
+        return f;
+    }
+
+}
